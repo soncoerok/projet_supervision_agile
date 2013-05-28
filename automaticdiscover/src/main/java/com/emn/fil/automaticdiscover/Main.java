200b55f50fc94d07a5adaa9a16266e32956c58a2
Clemzd <clemzd@gmail.com>
2013-05-28 15:10:43 +0200

    Suppression des jars Amélioration de Nmap (générique)

--- automaticdiscover/src/main/java/com/emn/fil/automaticdiscover/Main.java
+++ automaticdiscover/src/main/java/com/emn/fil/automaticdiscover/Main.java
@@ -1,29 +1,38 @@
 package com.emn.fil.automaticdiscover;
 
+import java.io.IOException;
+
 import javax.swing.UIManager;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.apache.log4j.Logger;
 
+import com.emn.fil.automaticdiscover.dto.IP;
+import com.emn.fil.automaticdiscover.dto.IPMask;
 import com.emn.fil.automaticdiscover.ihm.Frame;
+import com.emn.fil.automaticdiscover.nmap.Nmap;
 
 public class Main {
-	
+
 	public static Logger log = Logger.getLogger(Main.class);
-	
-	// Test
+
 	public static void main(String[] args) {
-		
+
+		// Set System L&F
 		try {
-			// Set System L&F
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (Exception e) {
 			Main.log.trace("ERROR : Impossible de modifier le theme");
 		}
-		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext-base.xml");
 
-		// instantiate our spring dao object from the application context
-		// Connection connection = (Connection)ctx.getBean("connection");
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext-base.xml");
+		Nmap nmap = (Nmap) ctx.getBean("nmap");
+		IPMask ipMask = new IPMask(new IP("172.17.2.0"), 24);
+		try {
+			nmap.scanner(ipMask);
+		} catch (IOException e) {
+			System.out.println("Erreur lors du scan");
+		}
 
 		Frame frame = (Frame) ctx.getBean("frame");
 		frame.initFrame();
